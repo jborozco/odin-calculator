3cffd5054b52bb7d09f919672d2fca30613528fa
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-06-01 18:44:45 +0200

    Define the key in js

---
+++ script.js
@@ -0,0 +1,51 @@
+
+// define keys
+const b0 = document.getElementsById('b0');
+const b1 = document.getElementsById('b1');
+const b2 = document.getElementsById('b2');
+const b3 = document.getElementsById('b3');
+const b4 = document.getElementsById('b4');
+const b5 = document.getElementsById('b5');
+const b6 = document.getElementsById('b6');
+const b7 = document.getElementsById('b7');
+const b8 = document.getElementsById('b8');
+const b9 = document.getElementsById('b9');
+const bPlus = document.getElementsById('bPlus');
+const bMinus = document.getElementsById('bMinus');
+const bMultiply = document.getElementsById('bMultiply');
+const bDivide = document.getElementsById('bDivide');
+const bEqual = document.getElementsById('bEqual');
+const bDelete = document.getElementsById('bDelete');
+const bOnOff = document.getElementsById('bOnOff');
+
+// define screen fields
+const lastNumber = document.getElementsById('lastNumber');
+const currentNumber = document.getElementsById('currentNumber');
+const operator = document.getElementsById('operator');
+
+
+//Log first input a string
+
+
+defineAKey = function (id) {
+    this.id = id;
+}
+
+calculatorButtons.forEach(element => {
+
+});
+
+
+
+
+
+
+
+
+
+// On selection of an operator > log first string as a int and apply the operator
+// Log second input a string
+// On equal or other operator show result and push current input 
+
+
+//need to commit pseudocode
