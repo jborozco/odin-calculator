39eb96b6766b3b0fccef3f46cd0b526eecc0764b
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-06-02 18:20:15 +0200

    Add a 0 to current value when clicking on the key 0

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 
 // define keys
-const b0 = document.getElementsById('b0');
-const b1 = document.getElementsById('b1');
-const b2 = document.getElementsById('b2');
-const b3 = document.getElementsById('b3');
-const b4 = document.getElementsById('b4');
-const b5 = document.getElementsById('b5');
-const b6 = document.getElementsById('b6');
-const b7 = document.getElementsById('b7');
-const b8 = document.getElementsById('b8');
-const b9 = document.getElementsById('b9');
-const bPlus = document.getElementsById('bPlus');
-const bMinus = document.getElementsById('bMinus');
-const bMultiply = document.getElementsById('bMultiply');
-const bDivide = document.getElementsById('bDivide');
-const bEqual = document.getElementsById('bEqual');
-const bDelete = document.getElementsById('bDelete');
-const bOnOff = document.getElementsById('bOnOff');
+const b0 = document.querySelector('#b0');
+const b1 = document.querySelector('#b1');
+const b2 = document.querySelector('#b2');
+const b3 = document.querySelector('#b3');
+const b4 = document.querySelector('#b4');
+const b5 = document.querySelector('#b5');
+const b6 = document.querySelector('#b6');
+const b7 = document.querySelector('#b7');
+const b8 = document.querySelector('#b8');
+const b9 = document.querySelector('#b9');
+const bPlus = document.querySelector('#bPlus');
+const bMinus = document.querySelector('#bMinus');
+const bMultiply = document.querySelector('#bMultiply');
+const bDivide = document.querySelector('#bDivide');
+const bEqual = document.querySelector('#bEqual');
+const bDelete = document.querySelector('#bDelete');
+const bOnOff = document.querySelector('#bOnOff');
 
 // define screen fields
-const lastNumber = document.getElementsById('lastNumber');
-const currentNumber = document.getElementsById('currentNumber');
-const operator = document.getElementsById('operator');
+const lastNumber = document.querySelector('#lastNumber');
+const currentNumber = document.querySelector('#currentNumber');
+const operator = document.querySelector('#operator');
 
 
 //Log first input a string
+let currentNumberValue = "";
 
 
+currentNumber.innerHTML = "-";
 
-defineAKey = function (id) {
-    this.id = id;
+
+b0.onclick = function () { inputCurrentNumber(b0) }
+
+inputCurrentNumber = function (button) {
+
+    currentNumberValue = currentNumberValue + button.title;
+    currentNumber.innerHTML = currentNumberValue;
+    console.log(button)
 }
 
-calculatorButtons.forEach(element => {
 
-});
+
+
+
+
 
 
 
